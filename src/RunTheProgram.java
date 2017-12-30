2e8df2cfb675191dceb7710a0c100756df0de2ef
Vadim <the.rocky.vadim@gmail.com>
2017-12-30 13:40:16 +0200

    Create RunTheProgram.java

---
+++ src/RunTheProgram.java
@@ -0,0 +1,15 @@
+import View.MainFrame;
+
+import javax.swing.*;
+
+public class RunTheProgram {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainFrame();
+            }
+        });
+    }
+}
